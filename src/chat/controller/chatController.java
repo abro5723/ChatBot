015b8b7202d0c4e7998bdec5fc222e10190c94a0
Brown, Ashton <abro5723@gmail.com>
2015-10-21 09:55:38 -0600

    Changed it just a bit

--- src/chat/controller/chatController.java
+++ src/chat/controller/chatController.java
@@ -1,10 +1,17 @@
 package chat.controller;
 
+import chat.view.ChatView;
+
+/**
+ * Application controller for the project
+ * @author abro5723
+ * @version 1.0 10/21/15
+ */
 public class chatController 
 {
+	private ChatView myDisplay;
 	public void start()
 	{
 		
 	}
-
 }
---
+++ src/chat/view/ChatView.java
@@ -0,0 +1,21 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+
+public class ChatView 
+{
+	public void showResponse(String wordsFromSomewhere)
+	{
+		JOptionPane.showMessageDialog(null, wordsFromSomewhere);
+	}
+	
+	public String grabAnswer(String stuff)
+	{
+		String answer = "";
+		answer = JOptionPane.showInputDialog(null, stuff);
+		return answer;
+	}
+
+}
+
+
